9b15140dc649bf27e150e5491fb92161dc38562b
Amy Spears <amspears007@gmail.com>
2022-11-29 22:10:14 -0700

    Initial commit with tests run

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # ./lib/school.rb
+class School
+    attr_reader :start_time, :hours_in_school_day, :student_names
+    def initialize(start_time, hours_in_school_day)
+        @start_time = start_time
+        @hours_in_school_day = hours_in_school_day
+        @student_names = [] 
+    end
+    
+    def add_student_name(name)
+        @student_names << name
+    end
+
+    def end_time 
+        # require 'pry'; binding.pry
+        "#{@hours_in_school_day + @start_time.to_i}:00"
+    end
+    
+    def is_full_time?
+        @hours_in_school_day > 4
+    end
+
+    def standard_student_names
+        @student_names.map do |student_name|
+            student_name.capitalize
+            # require 'pry'; binding.pry
+        end
+    end
+end
+
+
+
+    
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -4,33 +4,34 @@ require './lib/school'
 
 RSpec.describe School do
   context 'Iteration 1' do
-    xit 'exists' do
+    it 'exists' do
       school = School.new('9:00', 7)
 
       expect(school).to be_a(School)
     end
 
-    xit 'has a start time' do
+    it 'has a start time' do
       school = School.new('9:00', 7)
 
       expect(school.start_time).to eq('9:00')
     end
 
-    xit 'has hours in a school day' do
+    it 'has hours in a school day' do
       school = School.new('9:00', 7)
 
       expect(school.hours_in_school_day).to eq(7)
     end
 
-    xit 'starts with no student names' do
+    it 'starts with no student names' do
       school = School.new('9:00', 7)
 
       expect(school.student_names).to eq([])
+      # require 'pry'; binding.pry
     end
   end
 
   context 'Iteration 2' do
-    xit 'can add student names' do
+    it 'can add student names' do
       school = School.new('9:00', 7)
 
       school.add_student_name('Aurora')
@@ -40,7 +41,8 @@ RSpec.describe School do
       expect(school.student_names).to eq(['Aurora', 'tim', 'megan'])
     end
 
-    xit 'can calculate end time' do
+    it 'can calculate end time' do
+      # The `end_time` method should add the `hours_in_school_day` to the `start_time` and return the time in 24 hour format. 
       school_1 = School.new('9:00', 7)
       school_2 = School.new('9:00', 3)
 
@@ -50,8 +52,27 @@ RSpec.describe School do
   end
 
   context 'Iteration 3' do
+  #   * A school is considered full time if it has more than 4 hours in its school day.  
+  # * The method `standard_student_names` will return a list of student names, capitalized ('megan' should become 'Megan').
+    it 'can check if school is full time' do
+      school = School.new('9:00', 7)
+  
+      expect(school.is_full_time?).to eq(true)
+    end
+
+    it 'can create student standard names' do
+      school = School.new('9:00', 7)
+
+      school.add_student_name('Aurora')
+      school.add_student_name('tim')
+      school.add_student_name('megan')
+
+      expect(school.standard_student_names).to eq(["Aurora", "Tim", "Megan"])
+      
+    end
     # Add your own tests that follow the interaction pattern for Iteration 3
   end
+  
 
   context 'Iteration 4' do
     # Add your own tests that follow the interaction pattern for Iteration 4
